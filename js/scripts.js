138d7d4f4527f11d9ce4a1f2c207fc8494e47ea4
Andrew <expandrew@gmail.com>
2017-04-05 15:26:57 -0700

    get our javascript working so as to alter the price of the pizza depending on user selections.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,54 @@
 //business logic
-function Pizza (size, toppings)  {
-  this.measure = measure;
-  this.toppings = [];
-  this.price = {};
+function Pizza (inches, toppings, price)  {
+  this.inches = inches;
+  this.toppings = toppings;
+  this.price = price;
 };
 
 Pizza.prototype.price = function()  {
 
 };
 
-// var cartTotal = {};
+// var orderTotal = {};
 
 //user interface logic
 $(document).ready(function(event){
   $("form#byopizza").submit(function(event){
     event.preventDefault();
-      var measure = $("#measure").val();
-      $("input:checkbox[name=toppings]:checked").each(function(){
-        var toppings = $(this).val();
-      })
-      var price = {
-        if (measure === "9in")  {
-          //i am working right here
-        }
-      }
-    var newPizza = new Pizza(measure, toppings, price);
-  })
+      var inches = $("#inches").val();
+      var price = 0;
+        if (inches === "9in") {
+          price = price += 9.00;
+        } else if (inches === "12in")  {
+          price = price += 12.00;
+        } else if (inches === "16in") {
+          price = price += 16.00;
+        } else if (inches === "20in") {
+          price = price += 20.00;
+        };
+      var spread = $("#spreads").val();
+        if (spread === "Truffle Oil" || spread === "Basil Pesto" || spread === "BBQ" || spread === "Buffalo" || spread === "Alfredo") {
+          price = price += 1.00;
+        } else {
+          price = price;
+        };
+      var toppings = [];
+      $("input:checkbox[name=veggies]:checked").each(function(){
+        var selection = $(this).val();
+        toppings.push(selection);
+        price = price += 1.00;
+      });
+      $("input:checkbox[name=meats]:checked").each(function(){
+        var selection = $(this).val();
+        toppings.push(selection);
+        price = price += 1.50;
+      });
+      console.log(toppings);
+      console.log(price);
+      console.log(spread);
+      console.log(inches);
+    var newPizza = new Pizza(inches, toppings, price);
+    console.log (newPizza);
+  });
+
 });
