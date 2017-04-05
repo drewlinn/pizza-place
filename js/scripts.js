40852584bb565e6116909d2316d4c440dd0842c3
Andrew <expandrew@gmail.com>
2017-04-05 13:14:13 -0700

    begin implementing java and restructure certain page elements to better reflect project objectives.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 //business logic
+function Pizza (size, toppings)  {
+  this.measure = measure;
+  this.toppings = [];
+  this.price = {};
+};
+
+Pizza.prototype.price = function()  {
+
+};
+
+// var cartTotal = {};
 
 //user interface logic
 $(document).ready(function(event){
-
-})
+  $("form#byopizza").submit(function(event){
+    event.preventDefault();
+      var measure = $("#measure").val();
+      $("input:checkbox[name=toppings]:checked").each(function(){
+        var toppings = $(this).val();
+      })
+      var price = {
+        if (measure === "9in")  {
+          //i am working right here
+        }
+      }
+    var newPizza = new Pizza(measure, toppings, price);
+  })
+});
